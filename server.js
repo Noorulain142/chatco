2618b27c77bb377a1d1ab54bf543821f32873eca
Noor <noor.ulain@devsinc.com>
2023-09-19 13:32:32 +0500

    added web sockets for messaging on server-client side

--- server.js
+++ server.js
@@ -10,7 +10,23 @@ const io = socketio(server)
 app.use(express.static(path.join(__dirname,'public')))
 
 //run when client connects
-io.on('connection', socket =>{console.log('New Web Socket Connection...')} )
+io.on('connection', socket =>{
+// welcome current user
+socket.emit('message', 'Welcome to ChatCo' ) // single person who is connecting
+
+//broadcast when user connects
+socket.broadcast.emit('message', 'A user has joined the chat'); //emit to everpne except the user who is connecting
+
+//runs when client disconnects
+socket.on('disconnect', ()=> {
+  io.emit('message', 'A user has left the chat') // this used when we need to notify everyone
+})
+
+// listen for chat message
+socket.on('chatMessage', (msg)=> {
+  io.emit('message',msg)
+})
+} )
 
 const PORT = 3000 || process.env.PORT
 server.listen(PORT, () => console.log(`server running at port ${PORT}`))
