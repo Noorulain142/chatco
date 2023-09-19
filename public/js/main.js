6fed0d12fe4bc02d3e3f4ea1fadcb16c1807ae30
Noor <noor.ulain@devsinc.com>
2023-09-19 15:41:13 +0500

    joining and leaving chat rooms and messaging of multiple users

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,15 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages')
+
+//Get username and room from URL
+const { username, room } = Qs.parse(location.search,{
+  ignoreQueryPrefix: true  //to remove the '?' in front of query string
+})
+
 const socket = io();
+
+//Join chat room
+socket.emit('joinRoom', {username,room})
 
 //message from server
 socket.on('message', message =>{
@@ -27,14 +36,13 @@ chatForm.addEventListener('submit',(e)=> {
 
 })
 
-
 // output message to DOM
 function outputMessage(message){
   const div = document.createElement('div')
   div.classList.add('message')
-  div.innerHTML = `	<p class="meta">Brad <span>9:12pm</span></p>
+  div.innerHTML = `	<p class="meta">${message.username}<span>${message.time}</span></p>
   <p class="text">
-    ${message}
+    ${message.text}
   </p> `;
    document.querySelector('.chat-messages').appendChild(div);
 }
--- server.js
+++ server.js
@@ -2,30 +2,44 @@ const express = require('express')
 const path = require ('path')
 const http = require('http')
 const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
+const {userJoin, getCurrentUser,userLeave,getRoomUsers} = require('./utils/users')
+
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 //set static folder
 app.use(express.static(path.join(__dirname,'public')))
-
+const botName = 'chatCo Bot'
 //run when client connects
 io.on('connection', socket =>{
+socket.on('joinRoom',({username, room})=>{
+const user = userJoin(socket.id, username, room)
+socket.join(user.room)
+
 // welcome current user
-socket.emit('message', 'Welcome to ChatCo' ) // single person who is connecting
+socket.emit('message', formatMessage(botName,'Welcome to ChatCo!' ))// single person who is connecting
 
 //broadcast when user connects
-socket.broadcast.emit('message', 'A user has joined the chat'); //emit to everpne except the user who is connecting
+socket.broadcast.to(user.room).emit('message',formatMessage(botName, `${user.username} has joined the chat`)) //emit to everpne except the user who is connecting
 
-//runs when client disconnects
-socket.on('disconnect', ()=> {
-  io.emit('message', 'A user has left the chat') // this used when we need to notify everyone
 })
 
 // listen for chat message
 socket.on('chatMessage', (msg)=> {
-  io.emit('message',msg)
+const user = getCurrentUser(socket.id)
+  io.to(user.room).emit('message',formatMessage(user.username,msg))
 })
+
+//runs when client disconnects
+socket.on('disconnect', ()=> {
+  const user = userLeave(socket.id)
+  if(user){
+    io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`))
+  }
+})
+
 } )
 
 const PORT = 3000 || process.env.PORT
